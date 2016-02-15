660e45f8f067a87ef2f2213f40b59395ba8f45b0
Thomas D. Fischer <tdf.tomfischer@gmail.com>
2016-02-15 10:55:38 -0600

    In setup.py, py_modules -> packages. This fixes installation bugs. Upload to PyPI.

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(name="simpletcp",
       author="Thomas D. Fischer",
       author_email="tdf.tomfischer@gmail.com",
       url="http://gragas.github.io/simpletcp",
-      version="1.0.1",
+      version="1.0.2",
       license="Apache Version 2.0",
       description="Simple non-blocking TCP communcation.",
       keywords="tcp server non-blocking async asynchronous socket",
-      py_modules=["simpletcp",]
+      packages=["simpletcp",]
       )
